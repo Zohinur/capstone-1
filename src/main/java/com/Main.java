3eaf4f2861049970b6c79b8fa1a86d0d8689adfc
zohinurmiah <zohinur.m17@gmail.com>
2026-04-28 16:10:59 -0400

    Created objects from the csv

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,13 +1,28 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.Scanner;
 
-public class pluralsight {
+public class Main {
     static Scanner myScanner = new Scanner(System.in);
     static String userSelection;
-    static String filePath = "src/main/resources/Transaction.csv";
+    public static final String TRANSACTION_FILE_NAME = "src/main/resources/Transaction.csv";
+
+    static ArrayList<ledgerScreen> transactionFile = loadTransaction(TRANSACTION_FILE_NAME);
 
     public static void main(String[] args) {
+        mainMenu();
+    }
+
+    private static void mainMenu() {
         boolean running = true;
 
         do {
@@ -37,15 +52,56 @@ public class pluralsight {
                     System.err.println("Entered wrong syntax");
             }
         } while (running);
+    }
 
+    private static ArrayList<ledgerScreen> loadTransaction(String filePath) {
+
+        ArrayList<ledgerScreen> transaction = new ArrayList<ledgerScreen>();
+        try (
+                FileReader reader = new FileReader(filePath);
+                BufferedReader buffer = new BufferedReader(reader);
+        ) {
+            buffer.readLine();
+            String currentLine;
+            while ((currentLine = buffer.readLine()) != null) {
+                System.out.println("Here is the line" + currentLine);
+
+                String[] splitLine = currentLine.split("\\|");
+                LocalDate storeDate = LocalDate.parse(splitLine[0]);
+                LocalTime storeTime = LocalTime.parse(splitLine[1]);
+                String storeDescription = splitLine[2];
+                String storeVendor = splitLine[3];
+                double storeAmount = Double.parseDouble(splitLine[4]);
+
+                transaction.add(new ledgerScreen(storeDate, storeTime, storeDescription, storeVendor, storeAmount));
+            }
+
+        } catch (
+                IOException e) {
+            System.err.println("File is not found" + filePath);
+        }
+        return transaction;
     }
 
     private static void ledgerScreen() {
+
+
     }
+
 
     private static void makePayment() {
+
     }
 
-    private static void addDeposit() {
+    private static ArrayList<ledgerScreen> addDeposit() {
+        ArrayList<ledgerScreen> addDate = new ArrayList<ledgerScreen>();
+
+        System.out.println("Please enter the information for your Deposit transaction: ");
+
+        System.out.println("Please enter the date: ");
+        userSelection = myScanner.nextLine();
+        DateFormat dateFormat = userSelection;
+
     }
+
 }
--- src/main/java/com/ledgerScreen.java
+++ src/main/java/com/ledgerScreen.java
@@ -1,4 +1,62 @@
 package com;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class ledgerScreen {
+ public LocalDate date;
+ public LocalTime time;
+ public String description;
+ public String vendor;
+ public double amount;
+
+    public ledgerScreen(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.amount = amount;
+        this.vendor = vendor;
+        this.description = description;
+        this.time = time;
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
 }
